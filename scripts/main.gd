7b6cf59177e276026287f1f534f2fb41ff81f3b6
nirefi <2vwilkin@hbhs.school.nz>
2025-06-03 12:41:12 +1200

    panel

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,27 @@
 extends Control
 
 @onready var stats_label: Label = $VBoxContainer/PanelContainer/MarginContainer/StatsLabel
-var current_scene = ["browse", "offers", "storage"]
+@onready var main_content_panel: Panel = $VBoxContainer/MainContentPanel
+const ITEM_LIST = preload("res://scenes/item_list.tscn")
+const OFFERS_LIST = preload("res://scenes/offers_list.tscn")
 
 func _ready() -> void:
 	update_stats()
 
 func _on_browse_button_pressed() -> void:
-	# set current scene to be browse
-	current_scene[0]
+	hide_scenes_children(main_content_panel)
+	if main_content_panel.has_node("ItemList"):
+		print("TEST")
+	else:
+		var item_list_instance = ITEM_LIST.instantiate()
+		main_content_panel.add_child(item_list_instance)
+
+func _on_offers_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_storage_button_pressed() -> void:
+	# print the array of dictonary entries which is the storage. will not be adding proper display until finished minimum viable product
+	print(Global.items_held)
 
 func update_stats():
 	# set the stats
@@ -17,12 +30,7 @@ func update_stats():
 # TODO LATER: UPDATE STATS BY A SIGNAL SENT WHEN ANY BID BUTTON IS PRESSED AS TO MAKE THIS MORE PERFORMANT
 func _process(delta: float) -> void:
 	update_stats()
-
-
-func _on_storage_button_pressed() -> void:
-	# print the array of dictonary entries which is the storage. will not be adding proper display until finished minimum viable product
-	print(Global.items_held)
-
-
-func _on_offers_button_pressed() -> void:
-	pass # Replace with function body.
+	
+func hide_scenes_children(scene: Node) -> void:
+	for child in scene.get_children():
+		child.visible = false
